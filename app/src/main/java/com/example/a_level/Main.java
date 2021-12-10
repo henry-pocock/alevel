8ef1b795a1c0058f3080e5e3f62b677ce800a715
henry-pocock <henry@thistlecottage.co.uk>
2021-12-10 18:09:30 +0000

    Builds and runs, completely wrong

--- app/src/main/java/com/example/a_level/Main.java
+++ app/src/main/java/com/example/a_level/Main.java
@@ -37,7 +37,6 @@ public class Main extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
         return true;
     }
