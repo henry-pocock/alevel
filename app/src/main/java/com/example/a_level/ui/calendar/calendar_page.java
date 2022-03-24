ee3c9c0d05f47c88d4895c7a37d304f0e69377a2
henry-pocock <henry@thistlecottage.co.uk>
2022-03-24 09:38:04 +0000

    New event interface fixed days events fragment all logic code in page_calendar.xml

--- app/src/main/java/com/example/a_level/ui/calendar/calendar_page.java
+++ app/src/main/java/com/example/a_level/ui/calendar/calendar_page.java
@@ -1,48 +1,43 @@
 package com.example.a_level.ui.calendar;
 
+import static com.example.a_level.ui.calendar.new_event_interface.event_title;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.content.Intent;
-
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.a_level.Main;
 import com.example.a_level.databinding.PageCalendarBinding;
-
-import com.example.a_level.ui.errorreport_page;
 import com.skyhope.eventcalenderlibrary.CalenderEvent;
-import com.skyhope.eventcalenderlibrary.listener.CalenderDayClickListener;
-import com.skyhope.eventcalenderlibrary.model.DayContainerModel;
 import com.skyhope.eventcalenderlibrary.model.Event;
-
-import java.util.Calendar;
 import android.widget.Button;
+import java.util.Calendar;
 
-import org.w3c.dom.Text;
 
 public class calendar_page extends Fragment {
     private PageCalendarBinding binding;
+    public static String eventName;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        calendar_view_model calendarViewModel = new ViewModelProvider(this).get(calendar_view_model.class);
-
-        //calls the calendar view model, not in use at this point
 
         binding = PageCalendarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        Button button = binding.eventButton;
+        Button new_event_button = binding.eventButton;
 
-        button.setOnClickListener(new View.OnClickListener() {
+        //this is effectively an onCLickListener set for each day on the calendar,
+        //which will once clicked display the event set on the selected day
+        binding.calendarEvent.initCalderItemClickCallback(dayContainerModel -> {
+            if (dayContainerModel.isHaveEvent()) {
+                eventName = dayContainerModel.getEvent().getEventText();
+            } else {
+                Log.d("gemma!", "No event");
+            }
+        });
+
+        new_event_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(view.getContext(), new_event_interface.class));
@@ -51,27 +46,15 @@ public class calendar_page extends Fragment {
 
 
 
-
-        //sets the page view
-
-        //this is effectively an onCLickListener set for each day on the calendar,
-        //which will once clicked display the event set on the selected day
-        binding.calendarEvent.initCalderItemClickCallback(dayContainerModel -> {
-            TextView dayEvent = binding.daysEvents;
-            dayEvent.setText(""); //sets the text field as the event
-
-           if ( dayContainerModel.isHaveEvent()) {
-               dayEvent.setText(dayContainerModel.getEvent().getEventText()); //checks if there is an event
-           } else {
-               Log.d("tim", "No event"); //system response for event check
-           }
-
-
-        });
-
+        if (new_event_interface.check) {
+            CalenderEvent calenderEvent = binding.calendarEvent; //finding the calendar in the xml
+            Event event = new Event(Calendar.getInstance().getTimeInMillis(), event_title); //compiling the event data
+            calenderEvent.addEvent(event); //committing the event to the calendar
+        }
 
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
--- app/src/main/java/com/example/a_level/ui/calendar/days_event.java
+++ app/src/main/java/com/example/a_level/ui/calendar/days_event.java
@@ -1,22 +1,39 @@
 package com.example.a_level.ui.calendar;
 
+import static com.example.a_level.ui.calendar.calendar_page.eventName;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.a_level.databinding.EventDaysFragmentBinding;
 
-public class days_event extends Fragment {
 
+
+public class days_event extends Fragment {
     private EventDaysFragmentBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding.
 
+        binding = EventDaysFragmentBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        TextView dayEvent = binding.daysEvents;
+        dayEvent.setText(eventName);
+
+        return root;
     }
 
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
